a7620fd11b5bd15e4e7d54c5637c3b766e882ea8
Ashish <aasujoshiddl12@gmail.com>
2025-07-04 15:13:04 +0530

    add

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -273,13 +273,13 @@
                             <div class="col-xl-10 col-lg-9 ">
                                 <div class="footer-copy-right">
                                     <p>
-                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                        <!-- Link back to HFS can't be removed. Template is licensed under CC BY 3.0. -->
                                         Copyright &copy;<script>
                                         document.write(new Date().getFullYear());
                                         </script> All rights reserved | This template is made with <i
                                             class="fa fa-heart" aria-hidden="true"></i> by <a
-                                            href="https://colorlib.com" target="_blank">Colorlib</a>
-                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                            href="https://HFS.com" target="_blank">HFS</a>
+                                        <!-- Link back to HFS can't be removed. Template is licensed under CC BY 3.0. -->
                                     </p>
                                 </div>
                             </div>
