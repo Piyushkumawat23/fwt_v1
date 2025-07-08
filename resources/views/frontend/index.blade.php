e795d1f445084ff690e577ead05dd84b0639f77f
Ashish <aasujoshiddl12@gmail.com>
2025-07-08 15:16:43 +0530

    add

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -118,11 +118,11 @@
         <div class="service-area section-padding40">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-xl-6 col-lg-7 col-md-10 col-sm-10">
+                    <div class="col-xl-10 col-lg-7 col-md-10 col-sm-10">
                         <!-- Section Tittle -->
                         <div class="section-tittle text-center mb-80">
                             <span>What we are doing</span>
-                            <h2>We Are In A Mission To Help The Helpless</h2>
+                            <h2>Dedicated To Reviving Traditional Crafts And Empowering Skilled Hands</h2>
                         </div>
                     </div>
                 </div>
