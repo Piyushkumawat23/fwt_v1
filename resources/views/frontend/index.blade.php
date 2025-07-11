48a3f4c70cfe4dbc35c16d5a6ae25ec85febb755
Ashish <aasujoshiddl12@gmail.com>
2025-07-11 15:17:35 +0530

    add

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -394,7 +394,7 @@
                     <div class="col-lg-3 col-md-6 col-sm-6">
                         <div class="single-team mb-30">
                             <div class="team-img">
-                                <img src="{{ asset('public/assets/frontend/img/gallery/suresh-pareta-7.jpeg') }} " alt="Profile">
+                                <img src="{{ asset('public/assets/frontend/img/gallery/fwt1.jpg') }} " alt="Profile">
 
                                 <!-- Blog Social -->
                                 <ul class="team-social">
